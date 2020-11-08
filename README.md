1b0f85e29f2d016f09ed0b3cdd1cd858c74fe0d4
Nikolay <@doonn3>
2020-11-09 01:26:58 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,119 @@
+# Документация по GulpConfig v.0.5.0
+
+### Что такое Gulp `https://gulpjs.com/`
+
+**Gulp** — это таск-менеджер для автоматического выполнения часто используемых задач (например, [минификации](https://ru.wikipedia.org/wiki/%D0%9C%D0%B8%D0%BD%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%86%D0%B8%D1%8F_(%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5)) "Минификация (программирование)"), тестирования, объединения файлов), написанный на языке программирования JavaScript. Программное обеспечение использует командную строку для запуска задач, определённых в файле Gulpfile[[2]](https://ru.wikipedia.org/wiki/Gulp#cite_note-2). Создан как ответвление от проекта [Grunt](https://ru.wikipedia.org/wiki/Grunt_(%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%BD%D0%BE%D0%B5_%D0%BE%D0%B1%D0%B5%D1%81%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D0%B5)) "Grunt (программное обеспечение)"), чтоб взять из него лучшие практики[[3]](https://ru.wikipedia.org/wiki/Gulp#cite_note-3). Распространяется через менеджер пакетов [NPM](https://ru.wikipedia.org/wiki/NPM "NPM") под [MIT лицензией](https://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D1%86%D0%B5%D0%BD%D0%B7%D0%B8%D1%8F_MIT "Лицензия MIT")[[4]](https://ru.wikipedia.org/wiki/Gulp#cite_note-4).
+
+От Grunt отличается тем, что код задач записывается JavaScript кодом, а не в стиле конфигурационного файла[[5]](https://ru.wikipedia.org/wiki/Gulp#cite_note-5).
+
+На 2020-й год насчитывается более 3800 плагинов для Gulp[[https://gulpjs.com/plugins/ 1]](https://ru.wikipedia.org/wiki/Gulp#cite_note-6).
+
+Взаимодействия между частями программы реализуется через оператор `.pipe()`, выполняя по одной задаче за раз, не затрагивая исходные файлы, до конца процедуры. Это даёт возможность комбинации плагинов в любой последовательности и количестве.
+
+Так же в Gulp усовершенствована система сборки. Это значит, что помимо запуска задач, можно также копировать файлы с места на место, компилировать и развёртывать проект в новом окружении.
+
+---
+
+---
+
+**Как использовать Мой GulpConfig**
+
+Установка Node.js `https://nodejs.org/en/`
+
+после установки заходим в папку с проектом и в командной строке прописываем `npm i`, собирется проект со всеми плагинами и тд
+
+Запуск Gulp:
+
+заходим в папку с проектом и в командной строке прописываем `gulp`
+
+---
+
+---
+
+вся разработка проекта ведется в папке src/
+
+* src/
+  * fonts/названия файла.ttf и тд
+  * img/названия файла.jpg и тд
+  * js/component/
+    * script.js
+  * scss/component/
+    * style.scss
+  * html/component/
+    * index.html
+
+Как это работает пример: плагин(`gulp-file-include`)
+
+```
+путь src/html/index.html
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     @@include('_header.html') 
+</body>
+</html>
+```
+
+```
+путь src/component/header.html
+
+<header>
+    <h1>LOL</h1>
+</header>
+
+```
+
+Во время сборки файл header.html сольется с index.html:
+
+```
+путь src/html/index.html
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <header>
+         <h1>LOL</h1>
+     </header>
+</body>
+</html>
+```
+
+CSS:
+
+```
+путь src/scss/style.scss
+
+
+@import 'scss/component/button'
+```
+
+```
+путь src/scss/component/button.scss
+
+button {
+    color: red;
+    background-color: black;
+}
+```
+
+после сборки:
+
+```
+путь src/scss/style.scss
+
+button {
+    color: red;
+    background-color: black;
+}
+```
---
+++ gulpfile.js
@@ -0,0 +1,159 @@
+let sourceFolder = "src"; // название папки где идет разработка
+let projectFolder = "dist"; // название папки куда выгружает весь готовый проект 
+
+// путь в готовый проект
+let path = {
+  build: {
+    html: projectFolder + "/",
+    css: projectFolder + "/css/",
+    js: projectFolder + "/js/",
+    img: projectFolder + "/img/",
+    icons: projectFolder + "/icons",
+    fonts: projectFolder + "/fonts/",
+  },
+
+  // путь для разработки
+  src: {
+    html: [sourceFolder + "/html/**/*.html", "!" + sourceFolder + "/html/**/_*.html"],
+    css: sourceFolder + "/scss/style.scss",
+    js: sourceFolder + "/js/script.js",
+    img: sourceFolder + "/img/**/*.{jpg, png, svg, gif, ico, webp}",
+    icons: sourceFolder + "/icons/**/*.{svg, png}",
+    fonts: sourceFolder + "/fonts/*.ttf",
+  },
+
+  // следим
+  watch: {
+    html: sourceFolder + "/html/*.html", 
+    _html: sourceFolder + "/html/**/_*.html", //тестовый массив
+    css: sourceFolder + "/scss/**/*.scss",
+    js: sourceFolder + "/js/**/*.js",
+    img: sourceFolder + "/img/**/*.{jpg, png, svg, gif, ico, webp}",
+    icons: sourceFolder + "/icons/**/*.{svg, png}",
+  },
+
+  clean: "./" + projectFolder + "/", // удаляем папку
+};
+
+let { src, dest } = require("gulp"),
+  gulp = require("gulp"),            // Сам галп
+  browser_sync = require("browser-sync").create(), // Синхронизация с браузером автоматическое обновление после изменения в файлах src
+  fileinclude = require("gulp-file-include"), // путь пример подключения @@include('./view.html')
+  del = require("del"), 
+  scss = require("gulp-sass"), // Препроцессор sass для css
+  autoprefixer = require("gulp-autoprefixer"), //<--
+  group_media = require("gulp-group-css-media-queries"), // @media автоматически собираются и вставляются в дно файла
+  clean_css = require("gulp-clean-css"), // сжиматель css
+  rename = require("gulp-rename"), //<--
+  uglify = require("gulp-uglify-es").default, // сжиматель js
+  imagemin = require("gulp-imagemin"), // сжимаетль картинок
+  webp = require("gulp-webp"), // сэиматель и преобразования картинки .webp
+  webp_html = require("gulp-webp-html"); // автоматически подставляет тэг <pictures></pictures> для браузеров новых будут загружатся картинки с расширением .webp для браузеров старых .jpg и тд
+  //webp_css = require("gulp-webpcss") // gulp-webpcss не работает немогу пока понять как решить траблу
+
+
+// синхронизация с браузером
+function browserSync(params) {
+  browser_sync.init({
+    server: {
+      baseDir: "./" + projectFolder + "/"
+    },
+
+    port: 3000,
+    notify: false
+  });
+}
+
+function html() {
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(webp_html())
+    .pipe(dest(path.build.html))
+    .pipe(browser_sync.stream());
+}
+
+function css() {
+  return src(path.src.css)
+    .pipe(scss({
+      outputStyle: "expanded"
+    }))
+    .pipe(group_media())
+    .pipe(autoprefixer({
+      overrideBrowserslist: 'last 5 versions',
+      cascade: true
+    }))
+    //.pipe(webp_css()) //---------- 
+    .pipe(dest(path.build.css))
+    .pipe(clean_css())
+    .pipe(rename({
+      extname: ".min.css"
+    }))
+    .pipe(dest(path.build.css))
+    .pipe(browser_sync.stream());
+}
+
+function js() {
+  return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(uglify())
+    .pipe(rename({
+      extname: ".min.js"
+    }))
+    .pipe(dest(path.build.js))
+    .pipe(browser_sync.stream());
+}
+
+function images() {
+  return src(path.src.img)
+    .pipe(webp({
+       quality: 70 // Сжимает картинку формата webp от 0 до 100 где 100 без жатия 0 макс сжатие
+    }))
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false}],
+      interlaced: true,
+      optimizationLevel: 3     // Сжимает картинки 0 - без сжатия 7 - максимальное сжатие от 0 до 7
+    }))
+    .pipe(dest(path.build.img))
+    .pipe(browser_sync.stream());
+}
+
+function icons() {
+  return src(path.src.icons)
+      .pipe(dest(path.build.icons))
+      .pipe(browser_sync.stream());
+}
+
+// следим за файлами
+function watchFile(params) {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch._html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.img], images);
+  gulp.watch([path.watch.icons], icons);
+}
+
+// чистим путь
+function clean() {
+  return del(path.clean);
+}
+
+
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, icons));
+
+// выполняем слежку
+let watch = gulp.parallel(build, watchFile, browserSync);
+
+exports.icons = icons;
+exports.images = images;
+exports.js = js;
+exports.css = css;
+exports.html = html; 
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
+
